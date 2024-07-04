d992337ba1cb5aab6da5703386d8c155055ded13
Gayathri Madupu <gayathri.madupu@artihcus.com>
2024-07-04 05:23:34 +0000

    added assign and unassign

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -1,83 +1,140 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-   
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/model/json/JSONModel"
-],
-function (Controller, MessageToast, Filter, FilterOperator, JSONModel) {
+], function (Controller, MessageToast, Filter, FilterOperator, JSONModel) {
     "use strict";
 
     return Controller.extend("com.app.project1.controller.View1", {
         onInit: function () {
+            // Load local JSON model for the view
             var oModel = new JSONModel(sap.ui.require.toUrl("com/app/project1/data/model.json"));
             this.getView().setModel(oModel);
+        
 
+            // Set a second model (assuming it's defined in the manifest and Component.js)
             var oModelV2 = this.getOwnerComponent().getModel("ModelV2");
             this.getView().byId("pageContainer").setModel(oModelV2);
- 
-            
 
-            
+    // Initialize unassigned data model
+var oUnassignedModel = new JSONModel({ unassignedItems: [] });
+this.getView().setModel(oUnassignedModel, "unassignedModel");
+
+            if (!this.getView().getModel("tableModel")) {
+                var tableModel = new JSONModel();
+                this.getView().setModel(tableModel, "tableModel");
+            }
         },
 
         onItemSelect: function (oEvent) {
             var oItem = oEvent.getParameter("item");
             this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
+        },
+        onAssignPress: function () {
+            var oView = this.getView();
+            var oModel = oView.getModel("tableModel");
+        
+              // Ensure oModel is not undefined
+    if (!oModel) {
+        console.error("tableModel not found in the view.");
+        return;
+    }
+        
+            // Get values from input fields
+            var sDriverName = oView.byId("driverNameInput").getValue().trim();
+            var sPhone = oView.byId("driverPhoneInput").getValue().trim();
+            var sParkingLot = oView.byId("parkingLotInput").getValue().trim();
+            var sDateTime = oView.byId("dateTimePicker").getValue();
+            var sVehiclenum = oView.byId("Vehiclenumber").getValue();
+        
+            console.log("Driver Name:", sDriverName);
+            console.log("Phone:", sPhone);
+            console.log("Parking Lot:", sParkingLot);
+            console.log("Vehiclenum",sVehiclenum)
+            console.log("DateTime:", sDateTime);
+        
+            // Perform basic validation
+            if (!sDriverName || !sPhone || !sParkingLot || !sDateTime ||!sVehiclenum ) {
+                console.log("Validation failed: Please fill in all required fields.");
+                sap.m.MessageToast.show("Please fill in all required fields.");
+                return;
+            }
+        
+            // Assuming oModel contains an array of items to which assignments are added
+            var oNewAssignment = {
+                driverName: sDriverName,
+                drivernumber:sPhone,
+                vehicalDetails_vehicalNo: sVehiclenum, // Replace with actual vehicle number
+                plotNo_plot_NO: sParkingLot,
+                assignedDate: sDateTime
+            };
+        
+            // Add the new assignment to the model
+            var aItems = oModel.getProperty("/items") || [];
+            aItems.push(oNewAssignment);
+            oModel.setProperty("/items", aItems);
+        
+            // Show confirmation message
+            sap.m.MessageToast.show("Assignment added successfully");
+        
+            // Clear input fields after successful assignment
+            oView.byId("driverNameInput").setValue("");
+            oView.byId("driverPhoneInput").setValue("");
+            oView.byId("parkingLotInput").setValue("");
+            oView.byId("dateTimePicker").setValue("");
+            oView.byId("Vehiclenumber").setValue("");
+        },
+        onUnassignSinglePress: function (oEvent) {
+            var oItem = oEvent.getSource().getParent();
+            var oBindingContext = oItem.getBindingContext("tableModel");
+            var sPath = oBindingContext.getPath();
+            var oModel = this.getView().getModel("tableModel");
+        
+            var aItems = oModel.getProperty("/items");
+            var iIndex = parseInt(sPath.split("/").pop(), 10);
+            var oUnassignedItem = aItems.splice(iIndex, 1)[0];
+        
+            var oUnassignedModel = this.getView().getModel("unassignedModel");
+            var aUnassignedItems = oUnassignedModel.getProperty("/unassignedItems");
+        
+            oUnassignedItem.unassignedDate = new Date().toLocaleString();
+            aUnassignedItems.push(oUnassignedItem);
+            oUnassignedModel.setProperty("/unassignedItems", aUnassignedItems);
+        
+            oModel.setProperty("/items", aItems);
+            sap.m.MessageToast.show("Assignment removed successfully");
+        },
+        
+        onUnassignSelectedPress: function () {
+            var oView = this.getView();
+            var oTable = oView.byId("table4");
+            var oModel = oView.getModel("tableModel");
+            var aItems = oModel.getProperty("/items");
+        
+            var oUnassignedModel = oView.getModel("unassignedModel");
+            var aUnassignedItems = oUnassignedModel.getProperty("/unassignedItems");
+        
+            var aSelectedItems = oTable.getSelectedItems();
+            var aSelectedIndices = aSelectedItems.map(function (oItem) {
+                return oTable.indexOfItem(oItem);
+            });
+        
+            aSelectedIndices.sort(function (a, b) { return b - a; });
+        
+            aSelectedIndices.forEach(function (iIndex) {
+                var oUnassignedItem = aItems.splice(iIndex, 1)[0];
+                oUnassignedItem.unassignedDate = new Date().toLocaleString();
+                aUnassignedItems.push(oUnassignedItem);
+            });
+        
+            oUnassignedModel.setProperty("/unassignedItems", aUnassignedItems);
+            oModel.setProperty("/items", aItems);
+            oTable.removeSelections(true);
+        
+            sap.m.MessageToast.show("Selected assignments unassigned successfully");
         }
+        
     });
 });
-
-//         onLogin: function () {
-//             var oView = this.getView();
-
-//             var sUserID = oView.byId("usernameInput").getValue();
-//             var sPassword = oView.byId("passwordInput").getValue();
-
-//             if (!sUserID || !sPassword) {
-//                 MessageToast.show("please enter required Credentials");
-//                 return;
-//             }
-
-//             var oModel = this.getView().getModel();
-//             var oBinding = oModel.bindList("/UserLogin");
-
-//             oBinding.filter([
-//                 new Filter("userName", FilterOperator.EQ, sUserID),
-//                 new Filter("userpassword", FilterOperator.EQ, sPassword)
-//             ]);
-
-//             oBinding.requestContexts().then(function (aContexts) {  //requestContexts is called to get the contexts (matching records) from the backend.
-//                 debugger
-//                 if (aContexts.length > 0) {
-//                     var ID = aContexts[0].getObject().ID;
-//                     var userType = aContexts[0].getObject().typeOfUser;
-//                     var sUser = aContexts[0].getObject().userName;
-//                     if (userType === "Guard") {
-//                         MessageToast.show(sUser + " " + "Login Successful");
-//                         var oRouter = this.getOwnerComponent().getRouter();
-//                         oRouter.navTo("RouteGuard", { userName: ID });
-//                         var oView = this.getView()
-//                         oView.byId("usernameInput").setValue("");
-//                         oView.byId("passwordInput").setValue("");
-//                     }
-//                     else {
-//                         MessageToast.show("Login Successful");
-//                         var oRouter = this.getOwnerComponent().getRouter();
-//                         oRouter.navTo("Routesupervisor", { userName: ID });
-//                         var oView = this.getView()
-//                         oView.byId("usernameInput").setValue("");
-//                         oView.byId("passwordInput").setValue("");
-//                     }
-
-//                 } else {
-//                     MessageToast.show("Invalid username or password.");
-//                 }
-//             }.bind(this)).catch(function () {
-//                 MessageToast.show("An error occurred during login.");
-//             });
-//         }
-//     });
-// });
-
